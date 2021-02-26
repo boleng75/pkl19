864068cc7e5207cbcb176642b6f51e2e2738094c
jay150503maulana <fajarpro27@gmail.com>
2021-02-26 01:17:23 -0800

    yoooii

--- database/migrations/2021_01_16_010246_create_kasus2s_table.php
+++ database/migrations/2021_01_16_010246_create_kasus2s_table.php
@@ -16,9 +16,9 @@ class CreateKasus2sTable extends Migration
         Schema::create('kasus2s', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('id_rw');
-            $table->string('jumlah_positif');
-            $table->string('jumlah_meninggal');
-            $table->string('jumlah_sembuh');
+            $table->integer('jumlah_positif');
+            $table->integer('jumlah_meninggal');
+            $table->integer('jumlah_sembuh');
             $table->date('tanggal');
             $table->foreign('id_rw')->references('id')->on('rws')->onDelete('cascade');
             $table->timestamps();
